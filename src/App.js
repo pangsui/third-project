9da385d4267c90fe6fcb11ad19facbdc0c45dd59
pangsui <upangsuilingeh@gmail.com>
2024-11-26 05:06:30 +0100

    cleaning up of API after unmount

--- src/App.js
+++ src/App.js
@@ -13,87 +13,95 @@ import MovieDetails from "./components/MovieDetails";
 
 const KEY = "8d46b768";
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [query, setQuery] = useState("");
-  const [selectedId, setSelectedId] = useState(null);
-  const [watched, setWatched] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState("");
+	const [query, setQuery] = useState("");
+	const [selectedId, setSelectedId] = useState(null);
+	const [watched, setWatched] = useState([]);
 
-  function handleSelectMovie(id) {
-    setSelectedId((selectedId) => (id === selectedId ? null : id));
-  }
+	function handleSelectMovie(id) {
+		setSelectedId((selectedId) => (id === selectedId ? null : id));
+	}
 
-  function handleCloseMovie() {
-    setSelectedId(null);
-  }
+	function handleCloseMovie() {
+		setSelectedId(null);
+	}
 
-  function handleAddWatched(movie) {
-    setWatched([...watched, movie]);
-  }
+	function handleAddWatched(movie) {
+		setWatched([...watched, movie]);
+	}
 
-  function handleDeleteWatch(id) {
-    setWatched(watched.filter((watched) => watched.imdbID !== id));
-  }
+	function handleDeleteWatch(id) {
+		setWatched(watched.filter((watched) => watched.imdbID !== id));
+	}
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        setError("");
-        const res = await fetch(
-          ` https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-        );
-        if (!res.ok) throw new Error("Something went wrong.");
-        const data = await res.json();
-        if (data.Response === "False") throw new Error("Movie not found");
-        setMovies(data.Search);
-        setIsLoading(false);
-      } catch (err) {
-        console.error(err.message);
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    if (query.length === 0) {
-      setError("");
-      setMovies([]);
-      return;
-    }
-    fetchData();
-  }, [query]);
+	useEffect(() => {
+		// AbortController is a browser API  used to cancel the fetch request when the component is unmounted.
+		const controller = new AbortController();
+		async function fetchData() {
+			try {
+				setIsLoading(true);
+				setError("");
+				const res = await fetch(
+					` https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+					{ signal: controller.signal },
+				);
+				if (!res.ok) throw new Error("Something went wrong.");
+				const data = await res.json();
+				if (data.Response === "False") throw new Error("Movie not found");
+				setMovies(data.Search);
+				setError("");
+				setIsLoading(false);
+			} catch (err) {
+				console.error(err.message);
+				if (err.name !== "AbortError") {
+					setError(err.message);
+				}
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		if (query.length === 0) {
+			setError("");
+			setMovies([]);
+			return;
+		}
+		fetchData();
+		// The controller.abort() method is called when the component is unmounted.
+		return () => controller.abort();
+	}, [query]);
 
-  return (
-    <>
-      <NavBar>
-        <Search query={query} setQuery={setQuery} />
-        <NumResults movies={movies} />
-      </NavBar>
-      <Main>
-        {query.length > 0 ? (
-          <Box>
-            {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
-            {isLoading && <Loader />}
-            {!error && !isLoading && (
-              <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
-            )}
-            {error && <ErrorMge message={error} />}
-          </Box>
-        ) : (
-          <StartSearching />
-        )}
-        {selectedId ? (
-          <MovieDetails
-            selectedId={selectedId}
-            onCloseMovie={handleCloseMovie}
-            onAddWatch={handleAddWatched}
-            watched={watched}
-          />
-        ) : (
-          <WatchedBox watched={watched} onDeleteWatched={handleDeleteWatch} />
-        )}
-      </Main>
-    </>
-  );
+	return (
+		<>
+			<NavBar>
+				<Search query={query} setQuery={setQuery} />
+				<NumResults movies={movies} />
+			</NavBar>
+			<Main>
+				{query.length > 0 ? (
+					<Box>
+						{/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+						{isLoading && <Loader />}
+						{!error && !isLoading && (
+							<MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+						)}
+						{error && <ErrorMge message={error} />}
+					</Box>
+				) : (
+					<StartSearching />
+				)}
+				{selectedId ? (
+					<MovieDetails
+						selectedId={selectedId}
+						onCloseMovie={handleCloseMovie}
+						onAddWatch={handleAddWatched}
+						watched={watched}
+					/>
+				) : (
+					<WatchedBox watched={watched} onDeleteWatched={handleDeleteWatch} />
+				)}
+			</Main>
+		</>
+	);
 }
